00c3361bb9ef6ed6e2c89410ffcd1e28fc1d6a7d
vembu-karthick <74291183+vembu-karthick@users.noreply.github.com>
2023-04-22 09:25:14 +0530

    Add files via upload

---
+++ time-locked-wallets/README.md
@@ -0,0 +1,7 @@
+# Time Locked Wallets
+
+Please find more details about this project in [this blog post](https://www.toptal.com/ethereum-smart-contract/time-locked-wallet-truffle-tutorial#distinguish-only-choice-engineers).
+
+This ÐApp is deployed on Rinkeby and published with GitHub pages here:https://github.com/vembu-karthick/.
+Note that you will need Metamask or alike to use it.
+# time-locked-wallet
---
+++ time-locked-wallets/contracts/TimeLockedWallet.sol
@@ -0,0 +1,58 @@
+pragma solidity ^0.5.16;
+
+import "./ERC20.sol";
+
+contract TimeLockedWallet {
+
+    address public creator;
+    address public owner;
+    uint256 public unlockDate;
+    uint256 public createdAt;
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor(
+        address _creator,
+        address _owner,
+        uint256 _unlockDate
+    ) public {
+        creator = _creator;
+        owner = _owner;
+        unlockDate = _unlockDate;
+        createdAt = now;
+    }
+
+    // keep all the ether sent to this address
+    function() external payable { 
+       emit Received(msg.sender, msg.value);
+    }
+
+    // callable by owner only, after specified time
+    function withdraw() onlyOwner public {
+       require(now >= unlockDate);
+       //now send all the balance
+       msg.sender.transfer(address(this).balance);
+       emit Withdrew(msg.sender, address(this).balance);
+    }
+
+    // callable by owner only, after specified time, only for Tokens implementing ERC20
+    function withdrawTokens(address _tokenContract) onlyOwner public {
+       require(now >= unlockDate);
+       ERC20 token = ERC20(_tokenContract);
+       //now send all the token balance
+       uint256 tokenBalance = token.balanceOf(address(this));
+       token.transfer(owner, tokenBalance);
+      emit WithdrewTokens(_tokenContract, msg.sender, tokenBalance);
+    }
+
+    function info() public view returns(address, address, uint256, uint256, uint256) {
+        return (creator, owner, unlockDate, createdAt, address(this).balance);
+    }
+
+    event Received(address from, uint256 amount);
+    event Withdrew(address to, uint256 amount);
+    event WithdrewTokens(address tokenContract, address to, uint256 amount);
+}
